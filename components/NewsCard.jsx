5fcd2baf4e97883ca5f43a7a94a185118d003f42
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2023-11-14 17:23:45 +0100

    new branch

---
+++ components/NewsCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NewsCard() {
+  return (
+    <div className="card">
+      <div className="img"></div>
+      <div className="text">
+        <div className="title">
+          <h1></h1> <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
